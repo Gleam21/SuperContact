66826ad09ba52c73678e12a4a48d69b16200abc4
Jaemin Jung <windyvill@gmail.com>
2019-11-15 17:46:17 +0900

    new

---
+++ Assets/Scripts/PagingManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PagingManager : MonoBehaviour, IBeginDragHandler, IEndDragHandler
+{
+    ScrollRect cachedScrollRect;
+    Coroutine moveCoroutine;
+
+    public ScrollRect CachedScrollRect
+    {
+        get
+        {
+            if (cachedScrollRect == null)
+            {
+                cachedScrollRect = GetComponent<ScrollRect>();
+            }
+            return cachedScrollRect;
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GridLayoutGroup gridLayoutGroup
+            = CachedScrollRect.content.GetComponent<GridLayoutGroup>();
+
+        CachedScrollRect.StopMovement();
+        
+        float pageWidth = -(gridLayoutGroup.cellSize.x + gridLayoutGroup.spacing.x);
+        int pageIndex 
+            = Mathf.RoundToInt(CachedScrollRect.content.anchoredPosition.x / pageWidth);
+        Debug.Log(pageIndex);
+
+        float targetX = pageIndex * pageWidth;
+
+    }
+
+    public void OnTest(Vector2 value)
+    {
+
+    }
+}
---
+++ Assets/Scripts/PopViewManager/AlertPopupViewManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertPopupViewManager : PopupViewManager
+{
+    public void OnClcikOK()
+    {
+        NavigationManager manager 
+            = GameObject.Find("Canvas").GetComponent<NavigationManager>();
+
+        Close();
+    }
+}
