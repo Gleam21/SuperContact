b8b4df6ce4607129dcbc16b31bf8d0893c0ccf7b
Jaemin Jung <windyvill@gmail.com>
2019-11-15 18:19:53 +0900

    판넬 코루틴
    판넬 이동하는 movecell 코루틴 추가

--- Assets/Scripts/PagingManager.cs
+++ Assets/Scripts/PagingManager.cs
@@ -24,15 +24,19 @@ public class PagingManager : MonoBehaviour, IBeginDragHandler, IEndDragHandler
     public void OnBeginDrag(PointerEventData eventData)
     {
 
+        if(moveCoroutine!=null) StopCoroutine(moveCoroutine);
+
     }
+
 
     public void OnEndDrag(PointerEventData eventData)
     {
         GridLayoutGroup gridLayoutGroup
             = CachedScrollRect.content.GetComponent<GridLayoutGroup>();
+   
 
         CachedScrollRect.StopMovement();
-        
+
         float pageWidth = -(gridLayoutGroup.cellSize.x + gridLayoutGroup.spacing.x);
         int pageIndex 
             = Mathf.RoundToInt(CachedScrollRect.content.anchoredPosition.x / pageWidth);
@@ -40,7 +44,35 @@ public class PagingManager : MonoBehaviour, IBeginDragHandler, IEndDragHandler
 
         float targetX = pageIndex * pageWidth;
 
+        if(pageIndex >- 9&& pageIndex <= gridLayoutGroup.transform.childCount - 1)
+        {
+            moveCoroutine = StartCoroutine(MoveCell(new Vector2(targetX, 0), 0.5f));
+        }
+
+
+
     }
+
+    IEnumerator MoveCell(Vector2 targetPos,float duration)
+    {
+
+        Vector2 initPos = CachedScrollRect.content.anchoredPosition;
+        float currentTime = 0;
+
+        while (currentTime < duration)
+        {
+            Vector2 newPos = initPos + (targetPos - initPos);
+
+            float newTime = currentTime / duration;
+            Vector2 destPos = new Vector2(Mathf.Lerp(initPos.x,newPos.x,newTime), newPos.y);
+            CachedScrollRect.content.anchoredPosition = destPos;
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+        
+    }
+
 
     public void OnTest(Vector2 value)
     {
